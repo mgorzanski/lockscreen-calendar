36579d26f08a946cf6a249ab694c259c74a39f00
Mateusz <gorzanski.mateusz@gmail.com>
2018-12-30 17:58:32 +0100

    add linear gradient

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import { StyleSheet } from 'react-native';
 
 import CalendarScreen from './screens/Calendar';
 
@@ -7,13 +9,22 @@ export default class App extends React.Component {
     super(props);
 
     this.state = {
-      currentScreenId: 1
+      currentScreenId: 1,
+      newNotificationsArrived: false
     };
   }
 
   render() {
     return (
-      { this.state.currentScreenId === 1 ? (<CalendarScreen />) : null}
+      <LinearGradient colors={['#FF9100', '#FFA726']} style={styles.linearGradient}>
+        { this.state.currentScreenId === 1 ? (<CalendarScreen />) : null }
+      </LinearGradient>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1
+  }
+});
--- src/screens/Calendar/index.js
+++ src/screens/Calendar/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { }
+import { View, Text } from 'react-native';
 
 export default class CalendarScreen extends React.Component {
   render() {
     return (
-
+      <View>
+        <Text>aaaaa</Text>
+      </View>
     );
   }
 }
